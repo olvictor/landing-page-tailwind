253218097db332b20f0fa7d08e4866b639ca5713
VictorOlv <victorolvdev@gmail.com>
2024-01-25 22:10:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <div className='bg-background-color h-screen px-40 py-10'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
